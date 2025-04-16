bf1e88c6e217ab449b0a629d2b91286a7ad2fb91
solanatraderalo <solanatrader1337@tutamail.com>
2025-04-16 18:21:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // важно для корректной генерации путей
   plugins: [react()],
   root: 'src',
   build: {
