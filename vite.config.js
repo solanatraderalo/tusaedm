a2fa5b734b12ec23f0fbb58e1b820e782f44f13d
solanatraderalo <solanatrader1337@tutamail.com>
2025-04-16 20:41:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // важно для корректной генерации путей
+  base: '/', // корректная генерация путей
   plugins: [react()],
   root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+  },
+  optimizeDeps: {
+    exclude: ['@coinbase/wallet-sdk'] // ⛔ отключаем Coinbase SDK
+  },
+  resolve: {
+    alias: {
+      // иногда может понадобиться, если есть ошибки с импортами
+      '@': '/src'
+    }
   }
 })
